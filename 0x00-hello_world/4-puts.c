a431387c9b79a1a765db112de9124f3f0191aa98
charles lemuel <charleslemuel0@gmail.com>
2022-04-11 01:10:59 -0700

    lol

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,20 +1,14 @@
 #include <stdio.h>
 
 /**
+ * main - Entry point
  *
- *  * main - main block
+ * Description: prints "Programming is like building a multilingual puzzle"
  *
- *   * Return: 0
- *
- *    */
-
+ * Return: Always 0 (Success)
+ */
 int main(void)
-
 {
-
-        puts("\"Programming is like building a multilingual puzzle");
-
-        return (0);
-
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
-
