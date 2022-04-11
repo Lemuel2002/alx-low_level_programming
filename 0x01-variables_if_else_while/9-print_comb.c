238828ea7f65da09be690e3334eb205625d49b3b
charles lemuel <charleslemuel0@gmail.com>
2022-04-11 01:49:36 -0700

    scrpit

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -3,7 +3,7 @@
 /**
  * main - print single digit numbers
  *
- * Desciption: print single digit numbers
+ * Description: print single digit numbers with commas
  *
  * Return: Always 0 (Success)
  */
@@ -17,6 +17,11 @@ int main(void)
 	while (i < 58)
 	{
 		putchar(i);
+		if (i != 57)
+		{
+			putchar(44);
+			putchar(32);
+		}
 		i++;
 	}
 
